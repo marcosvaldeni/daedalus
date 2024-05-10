0742cb78769830a61fdae2f79b122a9da707e90c
Marcos Lucas <marcosvaldeni@gmail.com>
2024-05-10 17:11:24 +0100

    Creating transaction table2

--- db/migrations/20240509201816_create-transactions.ts
+++ db/migrations/20240509201816_create-transactions.ts
@@ -1,10 +1,18 @@
-import type { Knex } from "knex";
-
+import type { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('transactions', (table) => {
+    table.uuid('id').primary()
+    table.text('title').notNullable()
+    table
+      .decimal('amount', 10, 2)
+      .notNullable()
+      .defaultTo(knex.fn.now())
+      .notNullable()
+    table.timestamp('created_at')
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('transactions')
 }
-
