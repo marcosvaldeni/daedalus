718ccf44e8dcba418fb59e9ef2746c7f72771146
Marcos Lucas <marcosvaldeni@gmail.com>
2024-05-14 19:00:59 +0100

    Testing transaction creation

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,7 +1,22 @@
-import { test, expect } from 'vitest'
+import { test, expect, beforeAll, afterAll } from 'vitest'
+import request from 'supertest'
+import { app } from '../src/app'
 
-test('example', () => {
-  const responseStatusCode = 201
+beforeAll(async () => {
+  await app.ready()
+})
 
-  expect(responseStatusCode).toEqual(201)
+afterAll(async () => {
+  await app.close()
+})
+
+test('example', async () => {
+  await request(app.server)
+    .post('/transactions')
+    .send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+    .expect(201)
 })
